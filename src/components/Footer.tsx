89eafd1308feddcb852c0fc662bfd3195703fb97
Philipp-Strobl <p.str.hh@icloud.com>
2022-07-10 23:10:20 +0200

    build fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,11 @@
+import Image from "next/image";
+
 export default function Footer() {
   return (
     <>
       <footer>
         Made with{" "}
-        <img src="/netliheart.svg" alt="Netlify Logo" className="logo" /> for
+        <Image src="/netliheart.svg" alt="Netlify Logo" className="logo" /> for
         you
       </footer>
     </>
