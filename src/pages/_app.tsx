6a2840ee5e9178722c9929fd2d9f8e73ab8b63c8
Philipp-Strobl <p.str.hh@icloud.com>
2022-07-23 01:39:56 +0200

    Layout removed

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,7 +13,6 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Next + Contentful Starter</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { useContext, useEffect } from "react";
+import Head from "next/head";
 
 import { fetchEntries } from "~utils/contentfulData";
 
@@ -7,7 +8,9 @@ import { HackyAction } from "~types/HackyAction";
 
 import { HackyActionContext, HackyActionContextType } from "~context/hacky-action-provider";
 
-import Home from "~layout/Home";
+import Header from "~components/Home/Header";
+import Main from "~components/Home/Main";
+import Footer from "~components/Home/Footer";
 
 export default function Index({ hackyAction }: HackyAction) {
   const { updateHackyActionContext } = useContext(HackyActionContext) as HackyActionContextType;
@@ -16,7 +19,17 @@ export default function Index({ hackyAction }: HackyAction) {
     updateHackyActionContext(hackyAction);
   }, []);
 
-  return <Home />;
+  return (
+    <>
+      <Head>
+        <title>Home</title>
+      </Head>
+
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
 }
 
 export async function getStaticProps() {
